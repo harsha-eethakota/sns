a8ad71b624f9c250cfba7bea850b436bae87b194
harsha-eethakota <harsha-eethakota>
2024-01-29 22:52:12 +0000

    Initial commit

---
+++ src/test/java/com/skytrax/SnsTest.java
@@ -0,0 +1,21 @@
+package com.skytrax;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class SnsTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
